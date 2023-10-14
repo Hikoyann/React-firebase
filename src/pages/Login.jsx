ae4c9d52e992aab6bc016ed2d81d1bcf24157091
Hikoyann <i21i022dl@ous.jp>
2023-10-15 00:09:15 +0900

    ✨ firebaseの導入とログイン機能の作成、ローカルストレージに保管する機能を追加

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,31 @@
 import Head from "next/head";
+import { auth, provider } from "../utils/firebase";
+import { signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 
 const Login = () => {
+  const [isAuth, setIsAuth] = useState(false);
+  const loginInWithGoogle = () => {
+    // googlログイン
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+    });
+  };
   return (
     <>
       <Head>
         <title>Login Page</title>
       </Head>
-
-      <h1 className="text-red-500">
-        あああああああああああああああああああああああああああああああああああああああああ
-      </h1>
+      <div>
+        <h1 className="text-red-500">ログインして始めよう！</h1>
+        <button
+          onClick={loginInWithGoogle}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Googleログイン
+        </button>
+      </div>
     </>
   );
 };
