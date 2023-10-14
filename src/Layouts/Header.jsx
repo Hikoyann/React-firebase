1d7fa0aab67c46abc98f2fa1d07713e2957ebae5
Hikoyann <i21i022dl@ous.jp>
2023-10-14 23:50:06 +0900

    🚸 デザインを変更しました。

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -9,13 +9,13 @@ const NAV_ITEMS = [
 
 export const Header = () => {
   return (
-    <header className="flex justify-center items-center border-b w-full h-24 mb-4">
+    <header className="flex justify-center items-center border-b w-full h-24 mb-4 bg-Amber-700">
       {NAV_ITEMS.map((item) => {
         return (
           <Link
             key={item.href}
             href={item.href}
-            className="inline-block py-2 px-6 text-xl hover:text-blue-500 focus:text-blue-500 active:text-blue-500"
+            className="inline-block py-2 px-6 text-xl text-gray-700 hover:text-blue-500 focus:text-blue-500 active:text-blue-500"
           >
             {item.label}
           </Link>
